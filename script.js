30b68f8799f65606cac7f85ad4b60667e65197e7
Viacheslav Tyshchuk <slava1000uk@gmail.com>
2022-11-07 01:51:19 +0100

    feat: add script for slider

---
+++ script.js
@@ -0,0 +1,49 @@
+const sliderList = document.querySelector('.slider__list');
+const sliderFrame = document.querySelector('.slider');
+const imgs = document.querySelectorAll('.img');
+let count = 0;
+let width;
+
+
+function moveSlider() {
+  sliderList.style.left ='-' + count * width + 'px';
+}
+
+
+function init() {
+  width = sliderFrame.offsetWidth;
+
+  sliderList.style.width = width * imgs.length + 'px';
+  imgs.forEach(item => {
+    item.style.width = width + 'px';
+  });
+
+  moveSlider(); // move slider using updated width
+}
+
+init();
+
+window.addEventListener('resize', function () {
+  init();
+});
+
+document.querySelector('.control.previous').addEventListener('click', function () {
+  if (count > 0) {
+    count--;
+    moveSlider();
+  } else {
+    count = imgs.length - 1;
+    moveSlider();
+  }
+})
+
+document.querySelector('.control.next').addEventListener('click', function () {
+  if (count < imgs.length - 1) {
+    count++;
+    moveSlider();
+  } else {
+    count = 0;
+    moveSlider();
+  }
+
+})
